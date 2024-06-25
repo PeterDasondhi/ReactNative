cc7808822b0d99c485264f7129cb1040f7ceb46d
Abhay Dasondhi <abhaykumardasondhi@gmail.com>
2024-06-25 16:49:36 +0530

    Add FlexBox

--- FirstDemoApp/App.js
+++ FirstDemoApp/App.js
@@ -1,24 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Alert } from 'react-native';
+import { Button, StyleSheet, Text, View, Alert, TextInput } from 'react-native';
 
 export default function App() {
-
-  const showAlert = () => {
-    Alert.alert("Hii Abhay", "Good to go with react native.");
-  };
-
   return (
-    <View style={styles.container}>
-      <Button title="Click here" onPress={showAlert}></Button>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Enter your name" />
+        <Button title="Submit" onPress={() => Alert.alert('Button pressed')} />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    padding: 50,
     flex: 1,
     backgroundColor: '#fff',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  textInput: {
+    width: '80%',
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    padding: 10,
+    marginRight: 10,
+    padding: 10,
+    shadowOffset: { width: 0, height: 2 },
   },
 });
